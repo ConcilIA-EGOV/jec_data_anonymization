c3cc257f0177384fe38492061d99dafda8446665
maykonmarcosjunior <maykonmjunior@gmail.com>
2023-11-14 18:31:46 -0300

    Inserindo o powerset

--- main.py
+++ main.py
@@ -4,7 +4,32 @@
 import glob
 import random
 import re
-import time
+
+
+def powerset(some_list):
+    # Returns all subsets of size 0 - len(some_list) for some_list
+    if len(some_list) == 0:
+        return [[]]
+
+    subsets = []
+    first_element = some_list[0]
+    remaining_list = some_list[1:]
+    # Strategy: get all the subsets of remaining_list. For each
+    # of those subsets, a full subset list will contain both
+    # the original subset as well as a version of the subset
+    # that contains first_element
+    for partial_subset in powerset(remaining_list):
+        subsets.append(partial_subset)
+        subsets.append([first_element] + partial_subset[:])
+
+    return subsets
+
+
+def get_powerset(vector):
+    # Function to get all the subsets of a given vector.
+    power_set = [" ".join(subset) for subset in powerset(vector)[1:]]
+    return power_set
+
 
 AUTHOR_TOKENS = [
     "autor:",
@@ -42,7 +67,8 @@ def starts_with_any_token_from_list(text, list_token):
 
 
 def find_proc_num(text):
-    tokens_find = re.findall(r"[0-9]+-[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+", text)
+    tokens_find = re.findall(r"[0-9]+-[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+",
+                             text)
     tokens_find2 = re.findall(r"[0-9]+\.[0-9]+\.[0-9]+-[0-9]+", text)
     # 090.09.003319-1
     if len(tokens_find) > 0 or len(tokens_find2) > 0:
@@ -62,18 +88,17 @@ def anonymize(text):
     Here we try to find the author name, defendant and process number.
     """
 
-
     # Flags to indicate whether we found a match or not for author, part
     found_author = False
     found_part = False
 
-    # Convert text to lowercase and remove spaces and newlines in the beggining and ending.
+    # Convert text to lowercase and remove spaces
+    # and newlines in the beginning and ending.
     proc_text = text.lower().strip()
 
     tokens = text.split(":")
     tokens_replace = {}
 
-    # 
     new_token = text
 
     # Remove the author from the text
@@ -83,35 +108,50 @@ def anonymize(text):
         found_author = True
 
         # TODO: check substring here.
-        tokens = [token.strip() for token in tokens[1:]] 
+        tokens = [token.strip() for token in tokens[1:]]
         for token in tokens:
-            tokens_replace[token] = "AUTOR"
+            subtokens = get_powerset(token.split(" "))
+            for subtoken in subtokens:
+                tokens_replace[subtoken] = "AUTOR"
 
     if starts_with_any_token_from_list(proc_text, PARTE_TOKENS):
         found_part = True
         new_token = tokens[0] + ": REU\n\n"
 
         tokens = [token.strip() for token in tokens[1:]]
-        
+
         # TODO: check substring here.
         for token in tokens:
-            tokens_replace[token] = "REU"
+            subtokens = get_powerset(token.split(" "))
+            for subtoken in subtokens:
+                tokens_replace[subtoken] = "REU"
 
     # Here we try to find and replace the process number
     found_num_proc, text_new = find_proc_num(text)
 
-    # if we found the process number, we 
+    # if we found the process number,
+    # we replace it with a token.
     if found_num_proc:
         new_token = text_new
 
     return found_author, found_part, found_num_proc, tokens_replace, new_token
 
 
+def replace_pattern(input_string, pattern, replacement):
+    # Create a case-insensitive
+    # regular expression pattern
+    regex_pattern = re.compile(re.escape(pattern), re.IGNORECASE)
+    # Use sub() method to replace the pattern
+    # with the specified replacement
+    result_string = regex_pattern.sub(replacement, input_string, count=0)
+    return result_string
+
+
 def main():
 
     # List files
     list_files = glob.glob("data/raw/*.txt")
-    
+
     # Shuffle the list of files, to avoid order bias issues.
     random.shuffle(list_files)
 
@@ -125,40 +165,42 @@ def main():
                 replace_tokens = {}
 
                 # Iterate over all lines of the file.
-                for line in fp: 
+                for line in fp:
 
                     # TODO: here it returns the author, part of process number
-                    found_author, found_part, found_num_proc, replace_token, text = anonymize(line)
+                    (found_author, found_part,
+                     found_num_proc, replace_token, text) = anonymize(line)
 
                     replace_tokens.update(replace_token)
 
-                    # Whether the author was found where or has already been found before.
+                    # Whether the author was found where
+                    # or has already been found before.
                     f_author_doc = f_author_doc or found_author
                     f_part_doc = f_part_doc or found_part
                     f_num_proc = f_num_proc or found_num_proc
 
                     # For each of the tokens to replace (in the whole text)
-                    # Try to replace in all cases (Uppercase, lowercase, capitalize, etc.)
-                    # TODO: Maykon: provavelmente, seria bom você dar uma checada no "replace_tokens"
-                    # TODO: Maykon: ela é um dict que guarda todas as substituições para a anonimização.
-                    # Seria o caso de adicionar neste dicionario os subconjuntos.
-                    for key in replace_tokens:
-                        def replace_pattern(input_string, pattern, replacement):
-                            # Create a case-insensitive regular expression pattern
-                            regex_pattern = re.compile(re.escape(pattern), re.IGNORECASE)
-
-                            # Use sub() method to replace the pattern with the specified replacement
-                            result_string = regex_pattern.sub(replacement, input_string)
-
-                            return result_string
-                        
-                        text = replace_pattern(text, key, replace_tokens[key] + " ")
+                    # Try to replace in all cases (Uppercase,
+                    # lowercase, capitalize, etc.)
+                    # TODO: Maykon: provavelmente, seria bom você dar uma
+                    # TODO: Maykon: checada no "replace_tokens"
+                    # TODO: Maykon: ela é um dict que guarda todas as
+                    # TODO: Maykon: substituições para a anonimização.
+                    # Seria o caso de adicionar neste
+                    # dicionario os subconjuntos.
+                    ordered_keys = list(replace_tokens.keys())
+                    ordered_keys.sort(key=len, reverse=True)
+                    for key in ordered_keys:
+                        text = replace_pattern(text, key,
+                                               replace_tokens[key] + " ")
 
                     if not text.lower().strip().startswith("trato"):
                         fp_out.write(text)
 
-                # Em alguns casos não foi possível encontrar o autor, partes e número do processo.
-                # TODO: checar quais processos o processo falhou e porque? Faltou algum padrão que não checamos?
+                # Em alguns casos não foi possível encontrar o autor,
+                # partes e número do processo.
+                # TODO: checar quais processos o processo falhou e porque?
+                # Faltou algum padrão que não checamos?
                 if not f_author_doc:
                     print("-" * 10, file_path, "-" * 10)
                     print("not found author")
